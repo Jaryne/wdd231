3a2c889b88f1b2e08e389e0fda4c3a16aa2b9160
Jaryne <man24035@byui.edu>
2025-04-09 00:28:33 +0800

    load lazy

--- chamber/scripts/items.js
+++ chamber/scripts/items.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 card.innerHTML = `
                     <h2>${item.name}</h2>
                     <figure>
-                        <img src="${item.image}" alt="${item.name}">
+                        <img src="${item.image}" alt="${item.name}" loading="lazy">
                     </figure>
                     <address>${item.address}</address>
                     <p>${item.description}</p>
